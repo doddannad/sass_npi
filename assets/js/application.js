bc4c514b72019af1c9b8f6c467384209e35d4433
doddanna d <chawannaikd@gmail.com>
2021-05-27 01:45:19 +0530

    City Grid and Hot deals done

--- assets/js/application.js
+++ assets/js/application.js
@@ -3,5 +3,25 @@ $(document).ready(function() {
     if (index_page.length > 0) {
         // index page related only
         console.log("Index Page");
+        $('.hot-projects-carousel').owlCarousel({
+            animateOut: 'slideOutDown',
+            animateIn: 'flipInX',
+            loop: true,
+            autoplay: true,
+            margin: 20,
+            responsiveClass: true,
+            responsive: {
+                0: {
+                    items: 1
+                },
+                600: {
+                    items: 2
+                },
+                1000: {
+                    items: 3,
+                    dots: true
+                }
+            }
+        })
     }
 });
