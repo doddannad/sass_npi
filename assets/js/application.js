83e5c325a59a368c61fe35ec3c297dabd49a825e
doddanna d <chawannaikd@gmail.com>
2021-06-03 00:11:49 +0530

    Added Paginition to listing page and carousel indicators enabled

--- assets/js/application.js
+++ assets/js/application.js
@@ -3,64 +3,73 @@ $(document).ready(function() {
     if (index_page.length > 0) {
         // index page related only
         console.log("Index Page");
-        $('.3-2-1-carousel').owlCarousel({
-            animateOut: 'slideOutDown',
-            animateIn: 'flipInX',
-            loop: true,
-            autoplay: true,
-            margin: 20,
-            responsiveClass: true,
-            responsive: {
-                0: {
-                    items: 1
-                },
-                600: {
-                    items: 2
-                },
-                1000: {
-                    items: 3,
-                    dots: true
+        (() => {
+            $('.3-2-1-carousel').owlCarousel({
+                loop: true,
+                autoplay: true,
+                margin: 20,
+                nav: true,
+                dots: false,
+                responsiveClass: true,
+                responsive: {
+                    0: {
+                        items: 1
+                    },
+                    600: {
+                        items: 2
+                    },
+                    1000: {
+                        items: 3
+                    }
                 }
-            }
-        })
-        $('.5-3-2-carousel').owlCarousel({
-            animateOut: 'slideOutDown',
-            animateIn: 'flipInX',
-            loop: true,
-            autoplay: true,
-            margin: 20,
-            responsiveClass: true,
-            responsive: {
-                0: {
-                    items: 2
-                },
-                600: {
-                    items: 3
-                },
-                1000: {
-                    items: 4,
-                    dots: true
+            });
+            $('.5-3-2-carousel').owlCarousel({
+                loop: true,
+                autoplay: true,
+                margin: 20,
+                nav: true,
+                dots: false,
+                responsiveClass: true,
+                responsive: {
+                    0: {
+                        items: 2
+                    },
+                    600: {
+                        items: 3
+                    },
+                    1000: {
+                        items: 4
+                    }
                 }
-            }
-        })
+            });
+        })();
     }
     proper_listing = $('.property-listing');
     if (proper_listing.length > 0) {
-        $('.carousel-single').owlCarousel({
-            loop: true,
-            autoplay: true,
-            margin: 10,
-            items: 1
-        })
+        (() => {
+            $('.carousel-single').owlCarousel({
+                loop: true,
+                autoplay: true,
+                margin: 10,
+                nav: true,
+                dots: false,
+                items: 1
+            })
+        })();
     }
     proper_single = $('.property-single');
     if (proper_single.length > 0) {
-        $('.carousel-single').owlCarousel({
-            loop: true,
-            autoplay: true,
-            margin: 10,
-            items: 1
-        })
+        (() => {
+            $('.carousel-single').owlCarousel({
+                loop: true,
+                autoplay: true,
+                margin: 10,
+                nav: true,
+                dots: false,
+                items: 1
+
+            })
+        })();
     }
 
 });
